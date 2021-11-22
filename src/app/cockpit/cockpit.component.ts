9be598811e8de0cd0af69bfad973666c6f4a63a9
Doka, Istvan (EXTERN: msg DAVID) <extern.istvan.doka@volkswagen.de>
2021-11-22 14:09:18 +0200

    Local html reference

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -9,7 +9,6 @@ export class CockpitComponent implements OnInit {
   @Output() serverCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{ serverName: string, serverContent: string }>();
 
-  newServerName = '';
   newServerContent = '';
 
   constructor() {
@@ -18,19 +17,19 @@ export class CockpitComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onAddServer() {
+  onAddServer(nameInput: HTMLInputElement) {
     this.serverCreated.emit(
       {
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent
       }
     )
   }
 
-  onAddBlueprint() {
+  onAddBlueprint(nameInput: HTMLInputElement) {
     this.blueprintCreated.emit(
       {
-        serverName: this.newServerName,
+        serverName: nameInput.value,
         serverContent: this.newServerContent
       }
     )
