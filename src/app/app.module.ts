1903d580b848630c13789c03eedaea2ccca970b2
Doka, Istvan (EXTERN: msg DAVID) <extern.istvan.doka@volkswagen.de>
2022-01-05 18:56:40 +0200

    Dynamic routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -39,6 +39,7 @@ import {Server11Component} from "./sec_11_routing/servers/server/server11.compon
 const appRoutes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'users', component: UsersComponent},
+  {path: 'users/:id/:name', component: UserComponent},
   {path: 'servers', component: Servers11Component}
 ];
 
