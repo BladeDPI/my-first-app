fce53da4e5a414943f400020b106b5fbe4319b45
Doka, Istvan (EXTERN: msg DAVID) <extern.istvan.doka@volkswagen.de>
2022-01-18 19:14:24 +0200

    Patch data, Get Data, reset data

--- src/app/sec_15_forms/form-template/form-template.component.ts
+++ src/app/sec_15_forms/form-template/form-template.component.ts
@@ -11,6 +11,14 @@ export class FormTemplateComponent implements OnInit {
   defaultQuestion = 'pet';
   answer = "";
   genders = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion:'',
+    anwer:'',
+    gender:''
+  }
+  submitted = false;
 
   constructor() {
   }
@@ -20,13 +28,36 @@ export class FormTemplateComponent implements OnInit {
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secrete: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      },
+      secrete: 'pet'
+    });
   }
 
-  onSubmit(form: NgForm){
+  onSubmit(form: NgForm) {
     console.log(form);
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secrete;
+    this.user.anwer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+    this.submitted = true;
+
+    this.signupForm.reset();
   }
 
-  onSubmit1(){
+  onSubmit1() {
     console.log(this.signupForm);
   }
 }
